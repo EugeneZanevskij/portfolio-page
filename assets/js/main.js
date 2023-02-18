daff60be5b3ac3e64f0c5c660faec5d628c2a1a3
EugeneZanevskij <zanevskijzenia@gmail.com>
2023-02-18 21:19:54 +0300

    feat: add home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,24 @@
 const navLinks = document.querySelectorAll(".nav__link");
 const toggle = document.querySelector(".nav__toggle");
 const navMenu = document.querySelector(".nav__menu");
-/*==================== MENU SHOW Y HIDDEN ====================*/
+
 navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", (e) => {
-      navLinks.forEach((navLinkItem) => {
-        navLinkItem.classList.remove("active");
-      });
-      e.target.classList.add("active");
+  navLink.addEventListener("click", (e) => {
+    navLinks.forEach((navLinkItem) => {
+      navLinkItem.classList.remove("active");
     });
+    e.target.classList.add("active");
   });
+});
 
-  toggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    toggle.classList.toggle("active");
-    navMenu.addEventListener("click", () => {
-        navMenu.classList.remove("active");
-      toggle.classList.remove("active");
-    });
+toggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  toggle.classList.toggle("active");
+  navMenu.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    toggle.classList.remove("active");
   });
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
+});
 
 
 /*==================== ACCORDION SKILLS ====================*/
